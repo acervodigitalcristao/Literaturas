ce3af791d744965496ec92a263efd8f0635044e9
7miguelsilva7 <7miguelsilva7@gmail.com>
2018-04-11 18:52:43 -0300

    update

--- atualizar-git.sh
+++ atualizar-git.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+#zip -r epub-ADC.zip epub/
+#zip -r mobi-ADC.zip mobi/
+zip -r pdf-ADC.zip pdf/
 git pull
 git add --all
 git commit -m "update"
 git push
 
-zip -r epub-ADC.zip epub/
-zip -r mobi-ADC.zip mobi/
-zip -r pdf-ADC.zip pdf/
+
